82ae182c5f437a2c69ce6d96a98186b0b96ff044
jalexkam <jalex.kam@gmail.com>
2014-02-20 00:12:45 +0000

    initial online contact set up

---
+++ Application/Application/Controllers/ContactController.vb
@@ -0,0 +1,13 @@
+﻿Namespace Application
+    Public Class ContactController
+        Inherits System.Web.Mvc.Controller
+
+        '
+        ' GET: /Contact
+
+        Function Index() As ActionResult
+            Return View()
+        End Function
+
+    End Class
+End Namespace
